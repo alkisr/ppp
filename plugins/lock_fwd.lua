23b7868882d05a07ae0e077e08092d52e07694c4
alkisr <4alsahr@gmail.com>
2017-01-25 14:21:10 +0200

    Add files via upload

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -1,56 +1,39 @@
---[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# Developer : Aziz < @TH3_GHOST > 
-# our channel: @DevPointTeam
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
+
 do
 
 local function pre_process(msg)
     
     --Checking mute
     local hash = 'mate:'..msg.to.id
-    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
+    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg)  then
+            local bss =  msg.from.first_name.."\n".."🔺 ممنوع اعادة التوجيه🔻".."\n".."💡 معرفك : @"..msg.from.username.."\n"
             delete_msg(msg.id, ok_cb, true)
-            send_large_msg(get_receiver(msg), 'عزيزي '..msg.from.first_name..'\nممنوع عمل اعادة توجيه من القنوات هنا التزم بقوانين المجموعة 👋👮\n#username @'..msg.from.username)
-            return "done"
+            return bss
         end
     
         return msg
     end
-
-  
-
-
-local function DevPoint(msg, matches)
+local function mohammed(msg, matches)
     chat_id =  msg.to.id
     
-    if is_momod(msg) and matches[1] == 'lock' then
-      
-            
+    if is_momod(msg) and matches[1] == 'قفل التوجيه' then
                     local hash = 'mate:'..msg.to.id
                     redis:set(hash, true)
-                    return ""
-  elseif is_momod(msg) and matches[1] == 'unlock' then
-                    local hash = 'mate:'..msg.to.id
-                    redis:del(hash)
-                    return ""
+                    return reply_msg(msg.id, 'تم ☑️ قفل 🔒 اعاده توجيه ✋😽', ok_cb, false)
+  elseif is_momod(msg) and matches[1] == 'فتح التوجيه' then
+      local hash = 'mate:'..msg.to.id
+      redis:del(hash)
+      return reply_msg(msg.id, 'تم ☑️ فتح 🔓 اعاده توجيه ✋😽', ok_cb, false)
 end
 
 end
 
 return {
     patterns = {
-        '^[/!#](lock) fwd$',
-        '^[/!#](unlock) fwd$'
+        '^(قفل التوجيه)$',
+        '^(فتح التوجيه)$'
     },
-    run = DevPoint,
+    run = mohammed,
     pre_process = pre_process
 }
 end
