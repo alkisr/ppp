9062465f2dc62037cc8f0e1f32ba4b31c58ade12
alkisr <4alsahr@gmail.com>
2017-01-26 07:39:37 +0200

    Update Hello.lua

--- plugins/Hello.lua
+++ plugins/Hello.lua
@@ -1,10 +1,10 @@
 do-- by @Dev_ar
 local function Dev_ar(msg,matches)
 if matches[1] == "chat_add_user" then 
- local add = "🌹 أهلاً بك عزيزي 🌝❤️\n🎗 اسمك : " ..msg.action.user.first_name.."\n".."🎗 معرفك : @"..(msg.action.user.username or " " ).."\n".."🎗 ايديك : "..msg.action.user.id.."\n💞  قناتنا【 @ar_h97 】 🇮🇶 "
+ local add = "اهلا بك☺~ منور (ة) /n الاسم: " ..msg.action.user.first_name.."\n".."🎗 معرفك : @"..(msg.action.user.username or " " ).."\n"
 return add 
 elseif matches[1] == "chat_add_user_link" then
- local lin = "🌹 أهلاً بك عزيزي 🌝❤️\n🎗 اسمك : " ..msg.from.first_name.."\n".."🎗 معرفك : @"..(msg.from.username or "" ).."\n".."🎗 ايديك : "..msg.from.id.."\n💞  قناتنا【 @ar_h97 】 🇮🇶 "
+ local lin = "🌹 أهلاً بك عزيزي 🌝❤️\n🎗 اسمك : " ..msg.from.first_name.."\n".."🎗 معرفك : @"..(msg.from.username or "" ).."\n"
 return lin
 end
 if matches[1] == "chat_del_user" then
